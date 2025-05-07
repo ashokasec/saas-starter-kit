4113cd9cfbee929bddeed1a6c37f34ee4cc5ae4d
Shivam Gupta <108524973+ashokasec@users.noreply.github.com>
2025-05-07 11:51:53 +0530

    refactor: boilerplate code

---
+++ src/lib/fonts/index.ts
@@ -0,0 +1,16 @@
+import {
+    Bricolage_Grotesque,
+    Epilogue,
+    Inter,
+    Space_Grotesk,
+    AR_One_Sans,
+    Source_Code_Pro
+  } from "next/font/google";
+  
+  export const bricolage = Bricolage_Grotesque({ subsets: ["latin"] });
+  export const epilogue = Epilogue({ subsets: ["latin"] });
+  export const space_grtoesk = Space_Grotesk({ subsets: ["latin"] });
+  export const inter = Inter({ subsets: ["latin"] });
+  export const sourceCodePro = Source_Code_Pro({ subsets: ["latin"] });
+  export const arOneSans = AR_One_Sans({ subsets: ["latin"] });
+  
